d837999f730daff8ee456ede2681ff138fa4e915
Brenda Oliveira <brenda.saldanha@etec.sp.gov.br>
2021-08-23 22:40:04 -0300

    Finalizando o protótipo

--- zombie_dice.py
+++ zombie_dice.py
@@ -1,60 +1,83 @@
 import random
 
 #Rotina para receber os jogadores
-print("Bem-vindo ao zombie dice!")
+print("Bem-vindo ao zombie dice! \n")
+
+nom_jogadores = []
+nome = ""
 
 try:
-    qJogadores = int(input("Qual a quantidade de jogadores? "))
-    if qJogadores >= 2 and qJogadores <= 98:
-        print("Quantidade de jogadores atendida. Divirtam-se!")
+    num_jogadores = int(input("Qual a quantidade de jogadores? "))
+    if num_jogadores >= 2 and num_jogadores <= 98:
+        print("Quantidade de jogadores atendida. Divirtam-se! \n")
     else:
         print("Quantidade de jogadores inválida!")
+
+    for i in range(num_jogadores):
+        nome = input("Digite o nome do " + str(i + 1) + "° jogador: ").split()
+        nom_jogadores.append(nome)
 except ValueError:
-    print("Número inválido, tente novamente!")
-
+    print("Valor inválido, tente novamente!")
 #Criando os dados
-
-#Dados verdes
 dado_verde = 'CPCTPC'
-#Dados amarelos
 dado_amarelo = 'TPCTPC'
-#Dados vermelhos
 dado_vermelho = 'TPTCPT'
+dados = [dado_verde, dado_verde, dado_verde, dado_verde, dado_verde, dado_verde,
+         dado_amarelo, dado_amarelo, dado_amarelo, dado_amarelo,
+         dado_vermelho, dado_vermelho, dado_vermelho]
 
-#Sortear 3 dados
-sortear_dados = random.randint(1, 3)
+cerebro = 0
+passos = 0
+tiros = 0
+jogador_atual = 0
+dado_sorteado = []
 
-#Sortear faces
-if sortear_dados == 1:
-    sortear_verde = random.randint(1, 6)
-    if sortear_verde == 1:
-        print(dado_verde1)
-    elif sortear_verde == 2:
-        print(dado_verde2)
-    elif sortear_verde == 3:
-        print(dado_verde3)
-    elif sortear_verde == 4:
-        print(dado_verde4)
-    elif sortear_verde == 5:
-        print(dado_verde5)
-    else:
-        print(dado_verde6)
-elif sortear_dados == 2:
-    sortear_amarelo = random.randint(1, 4)
-    if sortear_amarelo == 1:
-        print(dado_amarelo1)
-    elif sortear_amarelo == 2:
-        print(dado_amarelo2)
-    elif sortear_amarelo == 3:
-        print(dado_amarelo3)
-    else:
-        print(dado_amarelo4)
-else:
-    sortear_vermelho = random.randint(1, 4)
-    if sortear_vermelho == 1:
-        print(dado_vermelho1)
-    elif sortear_vermelho == 2:
-        print(dado_vermelho2)
-    else:
-        print(dado_vermelho3)
+while True:
+    # Sorteando os dados e suas faces
+    print("\n")
+    print("É a vez do ", nom_jogadores[jogador_atual], "\n")
+    print("Dados sorteados:")
+    for i in range(0, 3):
+        sorteio = random.randint(0, 12)
+        sorteados = dados[sorteio]
+        if sorteados == 'CPCTPC':
+            print("Dado", str(i + 1) + ": verde.")
+        elif sorteados == 'TPCTPC':
+            print("Dado", str(i + 1) + ": amarelo.")
+        else:
+            print("Dado", str(i + 1) + ": vermelho.")
+        dado_sorteado.append(sorteados)
 
+    print("\n")
+    print("Faces sorteadas:")
+    for sorteados in dado_sorteado:
+        faces = random.randint(0, 5)
+        face = sorteados[faces]
+        if face == "C":
+            cerebro += 1
+            print("Você comeu um cérebro")
+        elif face == "P":
+            passos += 1
+            print("Uma vítima escapou")
+        else:
+            tiros += 1
+            print("Você levou um tiro")
+    print("\n")
+    continuar_passar = input("Digite C para continuar e P para passar a vez para o próximo jogador: ")
+    if continuar_passar == "C" or continuar_passar == "c":
+        dado_sorteado = []
+        print("Iniciando mais uma rodada do turno atual...")
+    elif continuar_passar == "P" or continuar_passar == "p":
+        jogador_atual += 1
+        cerebro = 0
+        passos = 0
+        tiros = 0
+        dado_sorteado = []
+
+        if jogador_atual == len(nom_jogadores):
+            print("Finalizando o jogo!")
+            break
+    else:
+        print("Houve um erro interno do sistema, estamos trabalhando para arrumá-lo. \n" 
+              "Finalizando o jogo!")
+        break
